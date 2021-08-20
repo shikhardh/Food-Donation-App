d95e806edabdce614e0a040c6f5d8891f5b7a626
shikhardh <>
2021-08-20 15:42:53 +0530

    Connected database with Mongoose

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
-const bodyParser = require('body-parser');
-const {MongoClient} = require('mongodb');
-const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+const { MongoClient } = require("mongodb");
+const mongoose = require("mongoose");
 
 const uri = "mongodb://localhost:27017/mydb";
 
-mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb://localhost:27017/foodDonate", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-    console.log("COnnected to db")
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("COnnected to db");
   // we're connected!
 });
 
@@ -20,43 +23,130 @@ const foodSchema = new mongoose.Schema({
     name: String,
     quantity: Number,
     location: String,
-    contact: Number
+    contact: Number,
+    type: String,
   });
 
-const Food = mongoose.model('Food', kittySchema);
+  // SEED DATA
+   const Food = mongoose.model("Food", foodSchema);
 
-const seedData = new Food({ name: 'Silence' });
+//   let seedData = new Food({
+//     name: "chicken",
+//     quantity: "2",
+//     location: "sydney",
+//     contact: "987654321",
+//     type: "food",
+//   });
+
+//   seedData.save(function (err, book){
+//       if (err) {
+//           console.log(err);
+//           res.send("Something went wrong");
+//       }
+//       console.log(`Data Added: ${seedData}`);
+//   })
+
+//   seedData = new Food({
+//     name: "milk",
+//     quantity: "3",
+//     location: "perth",
+//     contact: "987654321",
+//     type: "drink",
+//   });
+
+//   seedData.save(function (err, book){
+//       if (err) {
+//           console.log(err);
+//           res.send("Something went wrong");
+//       }
+//       console.log(`Data Added 2: ${seedData}`);
+//   })
+
+//   seedData = new Food({
+//     name: "Burger",
+//     quantity: "3",
+//     location: "perth",
+//     contact: "987654321",
+//     type: "food",
+//   });
+
+//   seedData.save(function (err, book){
+//       if (err) {
+//           console.log(err);
+//           res.send("Something went wrong");
+//       }
+//       console.log(`Data Added 3: ${seedData}`);
+//   })
 
 //app.set('views', path.join(__dirname, 'views'));
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//app.use(upload.array()); 
-app.use( express.static( "public" ) );
+//app.use(upload.array());
+app.use(express.static("public"));
 
-const food = {"details": [{"name": "chicken", "quantity": "2", "place": "sydney", "contact": "987654321", "type":"food"}, {"name": "bread", "quantity": "4", "place": "darwin", "contact": "987654321", "type":"food"}, {"name": "milk", "quantity": "3", "place": "perth", "contact": "987654321", "type":"drink"}, {"name": "milk", "quantity": "3", "place": "perth", "contact": "987654321", "type":"drink"}]}
+// const food = {
+//   details: [
+//     ,
+//     {
+//       name: "bread",
+//       quantity: "4",
+//       place: "darwin",
+//       contact: "987654321",
+//       type: "food",
+//     },
+//     {
+//       name: "milk",
+//       quantity: "3",
+//       place: "perth",
+//       contact: "987654321",
+//       type: "drink",
+//     },
+//     {
+//       name: "milk",
+//       quantity: "3",
+//       place: "perth",
+//       contact: "987654321",
+//       type: "drink",
+//     },
+//   ],
+// };
 
-app.get('/', (req, res) => {
-    res.render('index');
-})
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-app.get('/search', (req, res) => {
-    //results in a table
-    res.render('food', {food: food})
-})
+app.get("/search", async (req, res) => {
+  //results in a table
+  const data = await Food.find().sort({'location': 1});
+  const food = {'details': data}
+  //console.log(data);
+  res.render("food", { food: food });
+});
 
-app.get('/donate_food', (req, res) => {
-    // donate food add
-})
+app.get("/donate_food", (req, res) => {
+  // donate food add
+});
 
-app.post('/food_added', (req, res) => {
+app.post("/food_added", (req, res) => {
+  console.log(req.body);
+
+  res.redirect("/");
+});
+
+app.post("/food_request", (req, res) => {
     console.log(req.body);
-    res.redirect('/');
+    // add food in collection
+    res.send(`Requested successfully: ${req.body.location} -- ${req.body.person}`);
 })
 
-app.listen(3000, function() {
-    console.log('Listening on 3000')
+app.get("/search_request", (req, res) => {
+    // do
 })
+
+app.listen(3000, function () {
+  console.log("Listening on 3000");
+});
